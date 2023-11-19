993c645304d1d3121e045f6a859e7221e1055eb4
aroojfatima16 <151453793+aroojfatima16@users.noreply.github.com>
2023-11-19 07:30:53 -0800

    Add files via upload
    Arooj Fatima
    F23BSEEN1E02084

---
+++ program1.py
@@ -0,0 +1,13 @@
+#Calculate the multiplication and sum of two numbers
+number1 = 20
+number2 = 30
+if (number1*number2)<=1000:
+    print(number1*number2)
+else:
+    print(number1+number2)
+number1 =40
+number2 =30
+if (number1+number2)<=1000:
+     print(number1+number2)
+else:
+    print(number1+number2)
---
+++ program3.py
@@ -0,0 +1,5 @@
+def print_even_index_characters(str1): 
+	for i in range(0, len(str1), 2): 
+		print(str1[i]) 
+str1 = "pynative" 
+print_even_index_characters(str1)
